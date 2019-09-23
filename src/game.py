cab533d6a1159fb4892316c089d4617dcb2a5536
Henrique de Castilhos <henrique.castilhos@magrathealabs.com>
2019-09-23 12:53:22 -0300

    Gerando número aleatório

--- src/game.py
+++ src/game.py
@@ -1,3 +1,5 @@
+import random
+
 from src.adapters.console_display import ConsoleDisplay
 from src.adapters.keyboard_guesses import KeyboardGuesses
 from src.domain.display import Display
@@ -5,11 +7,15 @@ from src.domain.guess_my_number import GuessMyNumber
 from src.domain.guesses import Guesses
 
 
+def get_number():
+    return random.randint(1, 10)
+
+
 def game():
     display: Display = ConsoleDisplay()
     guesses: Guesses = KeyboardGuesses()
 
-    GuessMyNumber(3, guesses, display).play()
+    GuessMyNumber(get_number(), guesses, display).play()
 
 
 if __name__ == '__main__':
